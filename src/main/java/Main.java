4e4708de76fca7773280b439dc81716683b4bf78
ahartog <arjen.denhartog@axians.com>
2021-04-16 13:59:44 +0200

    add status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main{
 	public static void main(String[] args) throws InterruptedException{
 		System.out.println("Hello3");
+		System.out.println("about to sleep 30 seconds");
 		Thread.sleep(30000);
 		System.out.println("exit");
 	}
