e32239850fbf9921a4634cdc53ee0e4f69d5aba1
ahartog <arjen.denhartog@axians.com>
2021-04-16 13:58:39 +0200

    fix build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main{
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		System.out.println("Hello3");
 		Thread.sleep(30000);
 		System.out.println("exit");
